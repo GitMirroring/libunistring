48924dfa5f37e2381437b3d0e29012d179bf7dc9
Bruno Haible <bruno@clisp.org>
2025-06-05 01:31:15 +0200

    Update to the newest gnulib.
    * version.sh: Update.

--- version.sh
+++ version.sh
@@ -1,6 +1,6 @@
 # Version number and release date.
-VERSION_NUMBER=1.3
-RELEASE_DATE=2024-10-16      # in "date +%Y-%m-%d" format
+VERSION_NUMBER=1.3-20250605
+RELEASE_DATE=2025-06-05      # in "date +%Y-%m-%d" format
 
 # Version of gnulib that was used in this release.
-GNULIB_GIT_COMMIT=6aae11ee424765b10f15345bf51fe5a37c6896f4
+GNULIB_GIT_COMMIT=730d136ce6bccc0c353c9c66f3ed1b6bbfd9d33c
